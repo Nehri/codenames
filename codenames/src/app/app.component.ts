15f46834637d74a30ee698a8b0f5b85e74496b03
Charlie Kelman <ckelman@google.com>
2021-11-30 17:44:33 -0500

    atoring game state in database.  Need to fetch gamestate from database

--- codenames/src/app/app.component.ts
+++ codenames/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Card, CardType } from './types';
 import { WORDS } from './words';
 import * as uuid from 'uuid';
 import { Router } from '@angular/router'; 
+import { AngularFireDatabase } from '@angular/fire/database';
+
 
 @Component({
   selector: 'app-root',
@@ -17,11 +19,81 @@ export class AppComponent {
   };
   cards: Card[][];
 
-  constructor(private router: Router ) {
+  constructor(private router: Router,  private db: AngularFireDatabase ) {
   }
 
   startNewGame(){
+    const cards = this.generateCards();
+    const types = this.generateTypes();
     const gameId = uuid.v4();
+    const game = this.db.object(`games/${gameId}`);
+    game.set({cards, types});
     this.router.navigate([`/games/${gameId}`]);
+  }
+
+
+  private generateCards(): Card[][] {
+    const cardArray: Card[][] = [];
+    const cardIndexSet = new Set();
+    for (let i = 0; i < 5; i++) {
+      let cardRow: Card[] = [];
+      for (let j = 0; j < 5; j++) {
+        let cardIndex = this.getRandomWordIndex();
+        while (cardIndexSet.has(cardIndex)) cardIndex = this.getRandomWordIndex();
+
+        cardRow.push({word: WORDS[cardIndex], type: CardType.UNKNOWN});
+        cardIndexSet.add(cardIndex);
+      }
+      cardArray.push(cardRow);
+    }
+
+    return cardArray;
+  }
+
+  private getRandomWordIndex() {
+    return Math.floor(Math.random() * WORDS.length);
+  }
+
+  private generateTypes(): CardType[] {
+    const types = [
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.DEATH,
+    ];
+    const blueTeamHasExtraCard = Math.round(Math.random()) === 0;
+    
+    if (blueTeamHasExtraCard) {
+      types.push(CardType.TEAM_BLUE);
+    } else {
+      types.push(CardType.TEAM_RED);
+    }
+
+     this.shuffleArray(types);
+     return types;
+  }
+
+  private shuffleArray(array): void {
+     array.sort(() => Math.random() - 0.5);
   }
 }
--- codenames/src/app/game/game.component.ts
+++ codenames/src/app/game/game.component.ts
@@ -14,83 +14,16 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class GameComponent implements OnInit {
   gameId: string;
   cards: Card[][];
-  types: CardType[];
+  // types: Promise<CardType[]>;
 
   constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { 
-    this.cards = this.generateCards();
-    this.types = this.generateTypes();
     this.gameId = this.route.snapshot.paramMap.get('gameId');
-    const game = db.object(this.gameId);
-    game.set({cards: this.cards, types: this.types});
-
+    // const game = this.db.object(`games/${this.gameId}`);
+    // this.cards = game.query.get().then(snapshot => snapshot['cards']);
     console.log('in constructor');
   }
 
   ngOnInit(): void {
   }
 
-  private generateCards(): Card[][] {
-    const cardArray: Card[][] = [];
-    const cardIndexSet = new Set();
-    for (let i = 0; i < 5; i++) {
-      let cardRow: Card[] = [];
-      for (let j = 0; j < 5; j++) {
-        let cardIndex = this.getRandomWordIndex();
-        while (cardIndexSet.has(cardIndex)) cardIndex = this.getRandomWordIndex();
-
-        cardRow.push({word: WORDS[cardIndex], type: CardType.UNKNOWN});
-        cardIndexSet.add(cardIndex);
-      }
-      cardArray.push(cardRow);
-    }
-
-    return cardArray;
-  }
-
-  private getRandomWordIndex() {
-    return Math.floor(Math.random() * WORDS.length);
-  }
-
-  private generateTypes(): CardType[] {
-    const types = [
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_BLUE,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.TEAM_RED,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.NEUTRAL,
-      CardType.DEATH,
-    ];
-    const blueTeamHasExtraCard = Math.round(Math.random()) === 0;
-    
-    if (blueTeamHasExtraCard) {
-      types.push(CardType.TEAM_BLUE);
-    } else {
-      types.push(CardType.TEAM_RED);
-    }
-
-     this.shuffleArray(types);
-     return types;
-  }
-
-  private shuffleArray(array): void {
-     array.sort(() => Math.random() - 0.5);
-  }
 }
