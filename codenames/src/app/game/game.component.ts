e7e87e5d7731a65223fffe75b3adeb14b7312358
Michelle Ross <michelleross@google.com>
2021-11-30 17:09:22 -0500

    generate types

--- codenames/src/app/game/game.component.ts
+++ codenames/src/app/game/game.component.ts
@@ -3,6 +3,7 @@ import { Card, CardType } from '../types';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
 import { WORDS } from '../words';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 
 @Component({
@@ -13,10 +14,14 @@ import { WORDS } from '../words';
 export class GameComponent implements OnInit {
   gameId: string;
   cards: Card[][];
+  types: CardType[];
 
-  constructor(private route: ActivatedRoute,) { 
+  constructor(private route: ActivatedRoute, private db: AngularFireDatabase) { 
     this.cards = this.generateCards();
+    this.types = this.generateTypes();
     this.gameId = this.route.snapshot.paramMap.get('gameId');
+
+    console.log('in constructor');
   }
 
   ngOnInit(): void {
@@ -44,4 +49,46 @@ export class GameComponent implements OnInit {
     return Math.floor(Math.random() * WORDS.length);
   }
 
+  private generateTypes(): CardType[] {
+    const types = [
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_BLUE,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.TEAM_RED,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.NEUTRAL,
+      CardType.DEATH,
+    ];
+    const blueTeamHasExtraCard = Math.round(Math.random()) === 0;
+    
+    if (blueTeamHasExtraCard) {
+      types.push(CardType.TEAM_BLUE);
+    } else {
+      types.push(CardType.TEAM_RED);
+    }
+
+     this.shuffleArray(types);
+     return types;
+  }
+
+  private shuffleArray(array): void {
+     array.sort(() => Math.random() - 0.5);
+  }
 }
