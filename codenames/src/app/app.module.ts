10e2e132554922079d4198d971aebe5c16e94052
Michelle Ross <michelleross@google.com>
2021-11-30 14:05:57 -0500

    adding firebaseconfig

--- codenames/src/app/app.module.ts
+++ codenames/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- codenames/src/environments/environment.ts
+++ codenames/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyDxRVnv7lOe30ogfDKUZ367hWJXX2LkFYA",
+    authDomain: "y-codenames.firebaseapp.com",
+    databaseURL: "https://y-codenames-default-rtdb.firebaseio.com",
+    projectId: "y-codenames",
+    storageBucket: "y-codenames.appspot.com",
+    messagingSenderId: "211429199292",
+    appId: "1:211429199292:web:40bd68f65017307c0aefb9",
+    measurementId: "G-1D4LXT76HP"
+  },
 };
 
 /*
