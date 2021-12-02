83c1e4416dd0d3df297a99ebc0192d373aac098e
Michelle Ross <michelleross@google.com>
2021-12-02 09:40:52 -0500

    fixing merge

--- codenames/src/app/game-page/game-page.component.ts
+++ codenames/src/app/game-page/game-page.component.ts
@@ -1,15 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { Card, CardType, Team, TurnPhase } from '../types';
+import { Card, CardType, Team, TurnPhase, User } from '../types';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable, BehaviorSubject } from 'rxjs';
 import { first, switchMap, map, shareReplay} from 'rxjs/operators';
-=======
-import { Card, CardType, User } from '../types';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
-import { first, switchMap, map, shareReplay } from 'rxjs/operators';
->>>>>>> f4d77d6 (separate joining vs creating games)
 import { FormBuilder } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 
@@ -31,15 +24,12 @@ export class GamePageComponent implements OnInit {
   isInGame: Observable<boolean>;
   // types: Promise<CardType[]>;
   isCodeMaster: BehaviorSubject<boolean>;
-<<<<<<< HEAD
   turnPhase: Observable<any>;
   guessesRemaining: Observable<any>;
   teamTurn: Observable<any>;
   canClickCards: Observable<boolean>;
   canNotGiveClues: Observable<boolean>;
-=======
   user: Observable<User>;
->>>>>>> f4d77d6 (separate joining vs creating games)
 
   clueForm = this.formBuilder.group({
     word: '',
@@ -76,19 +66,15 @@ export class GamePageComponent implements OnInit {
           );
       })
     );
-<<<<<<< HEAD
-
     this.turnPhase = this.db.object(`games/${this.gameId}/currentTurn/phase`).valueChanges().pipe(shareReplay(1));
     this.canClickCards = this.turnPhase.pipe(map(gamePhase => gamePhase === TurnPhase.GUESSING), shareReplay(1));
     this.canNotGiveClues = this.turnPhase.pipe(map(gamePhase => gamePhase !== TurnPhase.CLUE_GIVING), shareReplay(1));
 
     this.guessesRemaining = this.db.object(`games/${this.gameId}/currentTurn/guessesRemaining`).valueChanges().pipe(shareReplay(1));
     this.teamTurn = this.db.object(`games/${this.gameId}/currentTurn/team`).valueChanges().pipe(shareReplay(1));
-=======
     this.isInGame = combineLatest([this.user, this.db.object(`games/${this.gameId}/players`).valueChanges()]).pipe(map(([user, players]) => {
       return !!players ? Object.keys(players).includes(user.uid ?? '') : false;
     }));
->>>>>>> f4d77d6 (separate joining vs creating games)
   }
 
   ngOnInit(): void {
