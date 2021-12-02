36e6bfb8846a8be61d04397b78d592e3e4c035b2
Michelle Ross <michelleross@google.com>
2021-12-02 10:18:01 -0500

    added player list

--- codenames/src/app/game-page/game-page.component.ts
+++ codenames/src/app/game-page/game-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Card, CardType, GameState, Team, TurnPhase, User } from '../types';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { first, switchMap, map, shareReplay} from 'rxjs/operators';
+import { first, switchMap, startWith, map, shareReplay} from 'rxjs/operators';
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { FormBuilder } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -19,9 +19,10 @@ import { AngularFireDatabase } from '@angular/fire/database';
 export class GamePageComponent implements OnInit {
   gameId: string;
   game: any;
-  gameState: Observable<GameState>;
+  gameState: any;
   cards: any;
   clues: any;
+  players: any;
   isInGame: Observable<boolean>;
   // types: Promise<CardType[]>;
   isCodeMaster: BehaviorSubject<boolean>;
@@ -46,7 +47,7 @@ export class GamePageComponent implements OnInit {
     this.gameId = this.route.snapshot.paramMap.get('gameId');
     this.user = afAuth.authState.pipe(shareReplay());
     this.isCodeMaster= new BehaviorSubject(false);
-    this.gameState = this.db.object(`games/${this.gameId}/gameState`).valueChanges().pipe(map(gameState => gameState ?? GameState.UNKNOWN));
+    this.gameState = this.db.object(`games/${this.gameId}/gameState`).valueChanges().pipe(map(gameState => gameState || GameState.UNKNOWN));
     this.clues = this.db.object(`games/${this.gameId}/clues`).valueChanges().pipe(map(obj => obj? Object.values(obj) : []));
     this.cards = this.isCodeMaster.pipe(
       switchMap(isCodeMaster => {
@@ -70,6 +71,13 @@ export class GamePageComponent implements OnInit {
           );
       })
     );
+    this.players = this.db.object(`games/${this.gameId}/players`).valueChanges().pipe(map((playerObject: any) => {
+      const playerArray = [];
+      for (const playerId in playerObject) {
+        playerArray.push({uid: playerId, email: playerObject[playerId]});
+      }
+      return playerArray;
+    }));
     this.turnPhase = this.db.object(`games/${this.gameId}/currentTurn/phase`).valueChanges().pipe(shareReplay(1));
     this.canClickCards = this.turnPhase.pipe(map(gamePhase => gamePhase === TurnPhase.GUESSING), shareReplay(1));
     this.canNotGiveClues = this.turnPhase.pipe(map(gamePhase => gamePhase !== TurnPhase.CLUE_GIVING), shareReplay(1));
